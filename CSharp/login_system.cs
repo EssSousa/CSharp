4c1c321c9d6db37f1ca31c97a5a754ecaebfcb98
FIRST_NAME LAST_NAME <eduardoguilhermesgamatesousa@gmail.com>
2023-03-11 21:14:35 +0000

    Adicionar arquivos de projeto.

---
+++ CSharp/login_system.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Login
+{
+    class person
+    {
+        public string name;
+        public string password;
+        public string email;
+    }
+
+    class Programa
+    {
+        static void Main()
+        {
+            person user = new person();
+            user.name = Console.ReadLine();
+            user.password = Console.ReadLine();
+            user.email = Console.ReadLine();
+        }
+    }
+}
